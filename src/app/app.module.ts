d19c42969ca06ab3d1fb3c7520128dd76ddee16f
Mouad MOUNACH <me.mounach@gmail.com>
2023-12-05 11:59:31 +0100

    feat: create atomic design architecture & init pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './pages/search/search.component';
+import { ReservationsComponent } from './pages/reservations/reservations.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { ThankYouComponent } from './pages/thank-you/thank-you.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ReservationsComponent,
+    CheckoutComponent,
+    ThankYouComponent
   ],
   imports: [
     BrowserModule
