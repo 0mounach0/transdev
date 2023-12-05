767771292b661d92d6c50c09501184118865f0f3
Mouad MOUNACH <me.mounach@gmail.com>
2023-12-05 19:46:00 +0100

    feat: service json data reader to pagedata

---
+++ src/app/services/data/data.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  public data!: any[];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public parseJSON(): Observable<any> {
+    let filePath: string = 'assets/data/data.json'; 
+    return new Observable(observer => {
+      this.httpClient.get(filePath).subscribe(
+        (jsonData: any) => {
+          observer.next(jsonData);
+          observer.complete();
+        },
+        error => {
+          observer.error(error);
+        }
+      );
+    });
+  }
+
+  public getPageData(pageNumber: number, pageSize: number): Observable<any[]> {
+    const startIndex = (pageNumber - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const pageData = this.data?.slice(startIndex, endIndex);
+    return of(pageData);
+  }
+}
