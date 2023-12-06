45e00209d019b15c52856feed74b15c36223a51f
Mouad MOUNACH <me.mounach@gmail.com>
2023-12-06 20:09:42 +0100

    tests: unit testing example on directive debounce

--- src/app/directives/debouce-keyup.directive.spec.ts
+++ src/app/directives/debouce-keyup.directive.spec.ts
@@ -1,8 +1,50 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { DebouceKeyupDirective } from './debouce-keyup.directive';
 
+@Component({
+  template: `
+    <input appDebouceKeyup (debounceKeyup)="onDebounceKeyup($event)">
+  `
+})
+class TestComponent {
+  onDebounceKeyup(event: Event) {
+    // Handle the event
+  }
+}
+
 describe('DebouceKeyupDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DebouceKeyupDirective();
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let inputEl: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DebouceKeyupDirective, TestComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    inputEl = fixture.debugElement.query(By.directive(DebouceKeyupDirective));
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should emit debounceKeyup event on keyup event', (done) => {
+    const input = inputEl.nativeElement as HTMLInputElement;
+    const event = new Event('keyup');
+    spyOn(component, 'onDebounceKeyup');
+    input.dispatchEvent(event);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.onDebounceKeyup).toHaveBeenCalledWith(event);
+      done();
+    });
   });
 });
